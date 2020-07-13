5d2c3d349c9638cf437c7f9b516eb5741a812e85
tvergho <tvergho@gmail.com>
2020-07-13 14:19:12 -0700

    Working header

--- src/components/business/business-header.js
+++ src/components/business/business-header.js
@@ -1,11 +1,28 @@
 import React from 'react';
 import Sticky from 'react-stickynode';
+import { BusinessLogo, ForMerchantsText } from './business-assets';
 
-const BusinessHeader = (props) => {
+const BusinessHeader = ({ scrollPosition, windowHeight }) => {
+  let styleClass = '';
+  let primaryColor = '';
+
+  if (Math.floor(scrollPosition / windowHeight) % 2 !== 0) {
+    styleClass = 'pink';
+    primaryColor = 'white';
+  } else {
+    styleClass = 'white';
+    primaryColor = '#FFB7B2';
+  }
+
   return (
-    <Sticky top="#business-header">
-      <div id="business-header">
-        <h1 style={{ color: 'red' }}>Sticky element</h1>
+    <Sticky innerZ={99}>
+      <div id="business-header" className={styleClass}>
+        <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'center' }}>
+          <BusinessLogo width="60px" height="60px" color={primaryColor} />
+          <ForMerchantsText style={{ marginLeft: '20px' }} color={primaryColor} />
+        </div>
+
+        <button type="button" className={`sign-in-button ${styleClass}`}>Sign up</button>
       </div>
     </Sticky>
   );
--- src/pages/business.js
+++ src/pages/business.js
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-unused-state */
 /* eslint-disable no-useless-constructor */
 /* eslint-disable react/prefer-stateless-function */
 import React, { Component, createRef } from 'react';
@@ -10,8 +11,15 @@ import {
 class Business extends Component {
   business = createRef();
 
+  businessContainer = createRef();
+
   constructor(props) {
     super(props);
+
+    this.state = {
+      windowHeight: 0,
+      scrollPosition: 0,
+    };
   }
 
   componentDidMount() {
@@ -21,23 +29,40 @@ class Business extends Component {
       duration: 150,
     });
     snapElement.bind(this.onSnap);
+
+    this.updateWindowDimensions();
+    window.addEventListener('resize', this.updateWindowDimensions);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.updateWindowDimensions);
   }
 
   onSnap = () => {
     console.log('snap');
   }
 
+  onScroll = () => {
+    this.setState({ scrollPosition: this.business.current.scrollTop });
+    console.log(this.business.current.scrollTop);
+  }
+
+  updateWindowDimensions = () => {
+    this.setState({ windowHeight: window.innerHeight });
+  }
+
   render() {
     return (
-      <>
-        <BusinessHeader />
+      <div onScroll={this.onScroll} ref={this.businessContainer}>
+        <BusinessHeader scrollPosition={this.state.scrollPosition} windowHeight={this.state.windowHeight} />
+
         <div id="business" ref={this.business}>
           <SEO title="Businesses" />
           <IntroPage />
           <LearnMorePage />
           <SignUpPage />
         </div>
-      </>
+      </div>
     );
   }
 }
